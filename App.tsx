3d6fa4ba1d144548c3834f579f8ac1ae4dd2f8f1
Anuj Sharma <anuj.sharma01@nagarro.com>
2022-08-22 14:38:46 +0530

    added tabs

--- App.tsx
+++ App.tsx
@@ -10,23 +10,65 @@
 
 import React from 'react';
 
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {HomeComponent} from './src/screens/home';
-import {DetailsComponent} from './src/screens/details';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { HomeComponent } from './src/screens/home';
+import { DetailsComponent } from './src/screens/details';
+import { Text, View } from 'react-native';
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
+function Profile() {
+  return (
+    <View>
+      <Text>profile screen</Text>
+    </View>
+  );
+}
+function Settings() {
+  return (
+    <View>
+      <Text>Settings screen</Text>
+    </View>
+  );
+}
+function Home() {
+  return (
+    <Tab.Navigator screenOptions={{ headerShown: true }}>
+      <Tab.Screen name="Profile" component={Profile} options={{ tabBarIcon: () => <Text>A</Text> }} />
+      <Tab.Screen name="Settings" component={Settings} options={{ tabBarIcon: () => <Text>B</Text> }} />
+      <Tab.Screen name="Settings2" component={Settings} options={{ tabBarIcon: () => <Text>C</Text> }} />
+      <Tab.Screen name="Settings3" component={Settings} options={{ tabBarIcon: () => <Text>D</Text> }} />
+    </Tab.Navigator>
+  );
+}
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
+      <Stack.Navigator initialRouteName='Home'>
+        {/* <Stack.Screen
           name="Home"
           component={HomeComponent}
-          options={{title: 'Overview'}}
-        />
-        <Stack.Screen name="Details" component={DetailsComponent} />
+          options={{ title: 'Overview' }}
+        /> */}
+        <Stack.Screen name='Home' component={Home} />
+        <Stack.Screen name="Details"
+          options={{
+            title: 'details screen',
+            headerStyle: {
+              backgroundColor: '#f4511e',
+            },
+            headerTintColor: 'white',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 30,
+              fontFamily: 'sans-serif-light'
+            },
+            headerTitleAlign: 'center'
+          }}
+          component={DetailsComponent} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -9,13 +9,26 @@ import {
   Text,
   useColorScheme,
   View,
+  RefreshControl,
+  TouchableOpacity
 } from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+const wait = (timeout: number) => {
+  return new Promise(resolve => setTimeout(resolve, timeout));
+}
 
 export const HomeComponent = (props: any) => {
   const isDarkMode = useColorScheme() === 'dark';
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    wait(2000).then(() => setRefreshing(false));
+  }, []);
 
   const onCLick = () => {
+    debugger
     console.log('clicked...');
     props?.navigation?.navigate('Details');
   };
@@ -25,19 +38,21 @@ export const HomeComponent = (props: any) => {
   };
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={true ? 'light-content' : 'dark-content'} />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
+        style={backgroundStyle}
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} tintColor={'blue'} colors={['blue']} progressBackgroundColor={'red'} />}
+      >
         <Text>
-          this is anuj's first app
-          <Button title="Go to Details" onPress={onCLick}></Button>
-          <Image
-            style={styles.image}
-            source={{
-              uri: 'https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg',
-            }}></Image>
+          <Text>this is anuj's first app</Text>
         </Text>
+        <Button title="Go to Details" onPress={onCLick}></Button>
+        <TouchableOpacity onPress={onCLick} style={{ padding: 10, backgroundColor: '#ddd' }} ><Text>Touch Me</Text></TouchableOpacity>
+        <Image
+          style={styles.image}
+          source={{
+            uri: 'https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg',
+          }}></Image>
       </ScrollView>
     </SafeAreaView>
   );
@@ -64,5 +79,6 @@ const styles = StyleSheet.create({
     height: 200,
     width: 400,
     flex: 1,
+    marginTop: 10,
   },
 });
